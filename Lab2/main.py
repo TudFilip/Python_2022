a54c366581edb34caa58aac1d9200d1a49de76df
TudFilip <filiptudormihail@yahoo.com>
2022-10-16 02:22:38 +0300

    rezolvare probleme de la 5 la 8

--- Lab2/main.py
+++ Lab2/main.py
@@ -11,7 +11,7 @@ def fibonacci(n):
 
 
 print('EX1: Lista primelor 10 numere din sirul Fibonacci: ', fibonacci(10))
-print('\n')
+print()
 
 
 '''
@@ -34,7 +34,7 @@ def primes_in_list(lst):
 
 
 print('EX2: Lista numerelor prime din lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]: ', primes_in_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-print('\n')
+print()
 
 
 '''
@@ -71,7 +71,7 @@ print("Lista [1, 2, 3, 4, 8, 9, 10] intersectata cu lista [1, 2, 3, 4, 5, 6, 7]:
 print("Lista [1, 2, 3, 4, 8, 9, 10] reunita cu lista [1, 2, 3, 4, 5, 6, 7]: ", a_reunited_b(list1, list2))
 print("Lista [1, 2, 3, 4, 8, 9, 10] minus lista [1, 2, 3, 4, 5, 6, 7]: ", a_minus_b(list1, list2))
 print("Lista [1, 2, 3, 4, 5, 6, 7] minus lista [1, 2, 3, 4, 8, 9, 10]: ", a_minus_b(list2, list1))
-print('\n')
+print()
 
 
 '''
@@ -91,7 +91,7 @@ def compose(musical_notes, moves, start_pos):
 
 
 print("EX4: ",  compose(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2))
-print('\n')
+print()
 
 
 '''
@@ -99,3 +99,119 @@ print('\n')
     elements under the main diagonal with 0 (zero).
 '''
 
+def change_matrix(matrix):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            if i > j:
+                matrix[i][j] = 0
+    return matrix
+
+
+def print_matrix(matrix):
+    i = 0
+    while i != len(matrix):
+        print(matrix[i])
+        i += 1
+
+
+test_matrix = [[1, 2, 3, 4, 6],
+     [5, 6, 7, 8, 10],
+     [9, 1, 2, 3, 7],
+     [4, 5, 6, 7, 5],
+     [2, 5, 10, 6, 7]]
+
+
+print("EX5:")
+print_matrix(change_matrix(test_matrix))
+print()
+
+
+'''
+    EX6: Write a function that receives as a parameter a variable number of lists and a whole number x. Return a list 
+    containing the items that appear exactly x times in the incoming lists. 
+'''
+
+def search(x, *lists):
+    final_list = []
+    for list in lists:
+        final_list += list
+
+    items = []
+    for number in final_list:
+        if final_list.count(number) == x and items.count(number) == 0:
+            items.append(number)
+    items.sort()
+    return items
+
+
+print("EX6: ", search(2, [1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"]))
+print()
+
+
+'''
+    EX7: Write a function that receives as parameter a list of numbers (integers) and will return a tuple with 2 
+    elements. The first element of the tuple will be the number of palindrome numbers found in the list and the second 
+    element will be the greatest palindrome number.
+'''
+
+def is_palindrome(x: int):
+    aux = x
+    pal = 0
+    while aux != 0:
+        pal = pal * 10 + aux % 10
+        aux = aux // 10
+
+    if x == pal:
+        return True
+    return False
+
+
+def palindromes_of_list(my_list: list):
+    counter = 0
+    greatest_palindrome = 0
+    for number in my_list:
+        if is_palindrome(number):
+            counter += 1
+            if number > greatest_palindrome:
+                greatest_palindrome = number
+
+    return counter, greatest_palindrome
+
+
+print("EX7: ", palindromes_of_list([123454321, 23451, 898989, 234432, 10001, 454]))
+print()
+
+
+'''
+    EX8: Write a function that receives a number x, default value equal to 1, a list of strings, and a boolean flag set 
+    to True. For each string, generate a list containing the characters that have the ASCII code divisible by x if the 
+    flag is set to True, otherwise it should contain characters that have the ASCII code not divisible by x.
+'''
+
+def string_characters(list_of_strings: list, flag: bool, x = 1):
+    final_list = []
+    for tmp_string in list_of_strings:
+        tmp_list = []
+        for char in tmp_string:
+            divisible = ord(char) % x
+            if flag:
+                if divisible == 0:
+                    tmp_list.append(char)
+            else:
+                if divisible != 0:
+                    tmp_list.append(char)
+        final_list.append(tmp_list)
+    return final_list
+
+
+print("EX8: ", string_characters(["test", "hello", "lab002"], False, 2))
+print()
+
+
+'''
+    EX9: Write a function that receives as paramer a matrix which represents the heights of the spectators in a stadium 
+    and will return a list of tuples (line, column) each one representing a seat of a spectator which can't see the 
+    game. A spectator can't see the game if there is at least one taller spectator standing in front of him. All the 
+    seats are occupied. All the seats are at the same level. Row and column indexing starts from 0, beginning with the
+    closest row from the field.
+'''
